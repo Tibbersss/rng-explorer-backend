9fbfcaf4fde4cb17a55b8bbb81d95eb0c7c1887e
tibbers <tibbers@vip.qq.com>
2019-04-09 16:24:42 +0800

    fix

--- app/router.js
+++ app/router.js
@@ -4,7 +4,7 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-  const { router, controller } = app;
+  const { router, controller, io} = app;
   router.get('/', controller.home.index);
   io.of('/').route('getLatestUnits', io.controller.rng.getLatestUnits);
 };
