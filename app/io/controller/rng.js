90ca4d5f2ef916e078161d07893a0aa756a1483d
tibbers <tibbers@vip.qq.com>
2019-04-10 14:06:59 +0800

    add io route getIndex

--- app/io/controller/rng.js
+++ app/io/controller/rng.js
@@ -13,6 +13,17 @@ class RngController extends Controller {
     });
     await ctx.socket.emit('UPDATE_LATEST_UNITS', data);
   }
+
+  async getIndex() {
+    const {ctx} = this;
+    let key = 'index';
+    let data = this.app.cache.get(key);
+    if (data === undefined) {
+      data = Date.now()
+      this.app.cache.set(key, data, 10);
+    }
+    await ctx.socket.emit('UPDATE_INDEX', data);
+  }
 }
 
 module.exports = RngController;
--- app/router.js
+++ app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller, io} = app;
   router.get('/', controller.home.index);
   io.of('/').route('getLatestUnits', io.controller.rng.getLatestUnits);
+  io.of('/').route('getIndex', io.controller.rng.getIndex);
 };
