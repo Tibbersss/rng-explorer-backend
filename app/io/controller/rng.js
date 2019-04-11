dcb9cbf750f3cc29faa75aa7259d18a3f81643d8
tibbers <tibbers@vip.qq.com>
2019-04-11 18:24:47 +0800

    update

--- app/io/controller/rng.js
+++ app/io/controller/rng.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const Controller = require('egg').Controller;
+const rpc = require('../../libs/rpc');
+
 
 class RngController extends Controller {
   async getLatestUnits() {
@@ -16,10 +18,22 @@ class RngController extends Controller {
 
   async getIndex() {
     const {ctx} = this;
+
     let key = 'index';
     let data = this.app.cache.get(key);
     if (data === undefined) {
-      data = Date.now()
+      data = {};
+      await rpc.get('miningStatus').then(res => {
+        data.miningStatus = JSON.parse(res.result);
+      })
+
+      data.latestUnits = await ctx.model.Units.findAll({
+        order: [
+          ['creation_date', 'DESC']
+        ],
+        limit: 10
+      });
+      console.log(data)
       this.app.cache.set(key, data, 10);
     }
     await ctx.socket.emit('UPDATE_INDEX', data);
